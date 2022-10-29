9f4b5896ad444d4a105af2de4e179250eed3e308
Leonardo Deganello de Souza <ra98995@uem.br>
2022-10-28 21:26:24 -0300

    correções

--- PC.sh
+++ PC.sh
@@ -11,7 +11,7 @@ thread=(2 3 4 5 6 7 8 9 10 11 12)
 for i in "${pthread_size[@]}"
 do
   printf "Tamanho da matriz $i\n"
-  for ((j = 0; j < 2; j++)); do
+  for ((j = 0; j < 3; j++)); do
     for ((k = 0; k < 15; k++)); do
       ./pthread $j $i 1
     done
